a706ebbaa982cabb8bbd9d98f4ddadb2ff366661
Rancho4507 <himanshukarn412@gmail.com>
2026-09-19 17:04:32 +0530

    Fix CORS origin URL

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ const allowedOrigins = [
     "http://localhost:3000",
     "http://127.0.0.1:5500",
     "https://citycare-frontend-5i4h.onrender.com",
-    "https://clean-my-city-vm.onrender.com"
+    "https://clean-my-city-vvmu.onrender.com"
 ];
 
 app.use(
